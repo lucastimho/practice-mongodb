661555566ae31eeed1d876dc952250fcfa1e0720
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-14 23:49:32 -0500

    adds database

--- app.js
+++ app.js
@@ -51,12 +51,12 @@ const insertDocuments = function(db, callback) {
 
 const findDocuments = function(db, callback) {
     // Get the documents collection
-    const collection = db.collection('document');
+    const collection = db.collection('fruits');
     // Find some documents
-    collection.find({}).toArray(function(err, docs){
+    collection.find({}).toArray(function(err, fruits){
         assert.equal(err, null);
         console.log("Found the following records");
-        console.log(docs)
-        callback(docs);
+        console.log(fruits)
+        callback(fruits);
     })
 }
