4ccdd5185492254e48369ce4d85fc3487fd510d1
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-15 19:49:57 -0500

    adds person model

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
 
 const fruitSchema = new mongoose.Schema({
     name: String,
-    ratin: Number,
+    rating: Number,
     review: String
 })
 
@@ -18,34 +18,19 @@ const fruit = new Fruit ({
 
 fruit.save();
 
-const insertDocuments = function(db, callback) {
-    // Get the documetns collection
-    const collection = db.collection('fruits');
-    //Inser some documents
-    collection.insertMany([
-        {
-            name: "Apple",
-            score: 8,
-            review: "Great fruit"
-        },
-        {
-            name: "Orange",
-            score: 6,
-            review: "Kind of sour"
-        },
-        {
-            name: "Banana",
-            score: 9,
-            review: "Great stuff"
-        }
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    })
-}
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+})
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person ({
+    name: "John",
+    age: 37
+})
+
+person.save();
 
 const findDocuments = function(db, callback) {
     // Get the documents collection
