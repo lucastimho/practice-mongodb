3e30b61b9fc88c78fee3ce90df3512a1cf10ec6c
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-15 19:40:27 -0500

    adds schema, model, and fruit

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
 
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    ratin: Number,
+    review: String
+})
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit"
+})
+
+fruit.save();
+
 const insertDocuments = function(db, callback) {
     // Get the documetns collection
     const collection = db.collection('fruits');
