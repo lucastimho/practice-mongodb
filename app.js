c6ef02228afd864b6f7d0d1a5f66d5534d244a97
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-15 19:34:58 -0500

    adds connection to mongoose

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'fruitsDB';
-
-//Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    client.close();
-})
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
 const insertDocuments = function(db, callback) {
     // Get the documetns collection
